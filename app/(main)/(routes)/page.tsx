c605f431bf4089671994973509a6d0af226e6c0a
anav5704 <anavkki@gmail.com>
2023-11-21 13:46:06 +1200

    init and clerk discord auth setup

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,7 +1,8 @@
+import { UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 
 export default function Home() {
-  return (
-    <h1>This is a protected route</h1>
-  )
+    return (
+        <UserButton afterSignOutUrl='/' />
+    )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,30 @@
-import type { Metadata } from 'next'
+import { ClerkProvider } from '@clerk/nextjs'
+import { ThemeProvider } from '@/providers/ThemeProvider'
 import { Open_Sans } from 'next/font/google'
+import type { Metadata } from 'next'
 import "@/app/globals.css"
 
 const font = Open_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Definitely Not Discord',
-  description: 'Fullstack discord clone made with Next JS 14, styled with Shadcn UI and TailwindCSS.',
+    title: 'Definitely Not Discord',
+    description: 'Fullstack discord clone made with Next JS 14, styled with Shadcn UI and TailwindCSS.',
 }
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={font.className}>{children}</body>
-    </html>
-  )
+    return (
+        <ClerkProvider>
+            <html lang="en">
+                <body className={font.className}>
+                    <ThemeProvider>
+                        {children}
+                    </ThemeProvider>
+                </body>
+            </html>
+        </ClerkProvider>
+    )
 }
